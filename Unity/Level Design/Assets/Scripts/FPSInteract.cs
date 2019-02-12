a21060ae6b27d49e6349c0241760cc2743d7429b
Unknown <genaralskar1@gmail.com>
2019-02-12 12:32:38 -0700

    add building modules, finish uving some, add lantern, lantern post, rope

--- Unity/Level Design/Assets/Scripts/FPSInteract.cs
+++ Unity/Level Design/Assets/Scripts/FPSInteract.cs
@@ -4,90 +4,95 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.Video;
 
-public class FPSInteract : MonoBehaviour
+namespace genaralskar.FPSInteract
 {
-
-    public float interactDistance = 2;
-    public string interactInput = "Interact";
-    public LayerMask layerMask;
-
-    private RaycastHit hit;
-    private Collider currentCollider;
-    private Ray ray;
+    public class FPSInteract : MonoBehaviour
+    {
     
-    void Update()
-    {
-        ray = new Ray(transform.position, transform.forward);
-        if (Physics.Raycast(ray, out hit, interactDistance, layerMask))
+        public float interactDistance = 2;
+        public string interactInput = "Interact";
+        public LayerMask layerMask;
+    
+        private RaycastHit hit;
+        private Collider currentCollider;
+        private Ray ray;
+        
+        void Update()
         {
-            //If input pressed while ray hit something send interact call
-            if (Input.GetButtonDown(interactInput))
+            ray = new Ray(transform.position, transform.forward);
+            if (Physics.Raycast(ray, out hit, interactDistance, layerMask))
             {
-                SendInteract(hit);
+                //If input pressed while ray hit something send interact call
+                if (Input.GetButtonDown(interactInput))
+                {
+                    SendInteract(hit);
+                }
+                
+                //if the collider the raycast hit is not the same as the stored collider
+                //send out StopLook call
+                //assign raycast collider as new collider
+                //send out StartLook call
+                if (hit.collider != currentCollider)
+                {
+                    if(currentCollider != null)
+                        SendStopLook();
+                    
+                    currentCollider = hit.collider;
+                    
+                    SendLookAt(hit);
+                }
             }
-            
-            //if the collider the raycast hit is not the same as the stored collider
+            //if ray didn't hit anything and there is a current collider
             //send out StopLook call
-            //assign raycast collider as new collider
-            //send out StartLook call
-            if (hit.collider != currentCollider)
+            //unassign currentCollider
+            else if(currentCollider != null)
             {
-                if(currentCollider != null)
-                    SendStopLook();
-                
-                currentCollider = hit.collider;
-                
-                SendLookAt(hit);
+                SendStopLook();
+                currentCollider = null;
             }
         }
-        //if ray didn't hit anything and there is a current collider
-        //send out StopLook call
-        //unassign currentCollider
-        else if(currentCollider != null)
+    
+        private void SendInteract(RaycastHit hit)
         {
-            SendStopLook();
-            currentCollider = null;
-        }
-    }
-
-    private void SendInteract(RaycastHit hit)
-    {
-        //Gets all IFPSInterat interfaces on the collided gameObject and sends out a call
-        var interacts = currentCollider.gameObject.GetComponents<IFPSInteract>();
-        foreach (var interact in interacts)
-        {
-            interact?.OnInteract();
-        }
-    }
-
-    private void SendLookAt(RaycastHit hit)
-    {
-        var interacts = currentCollider.gameObject.GetComponents<IFPSLookAt>();
-        foreach (var interact in interacts)
-        {
-            interact?.OnLook();
-        }
-    }
-
-    private void SendStopLook()
-    {
-        var interacts = currentCollider.gameObject.GetComponents<IFPSLookAt>();
-        {
+            //Gets all IFPSInterat interfaces on the collided gameObject and sends out a call
+            var interacts = currentCollider.gameObject.GetComponents<IFPSInteract>();
             foreach (var interact in interacts)
             {
-                interact?.OnStopLook();
+                interact?.OnInteract();
+            }
+        }
+    
+        private void SendLookAt(RaycastHit hit)
+        {
+            var interacts = currentCollider.gameObject.GetComponents<IFPSLookAt>();
+            foreach (var interact in interacts)
+            {
+                interact?.OnLook();
+            }
+        }
+    
+        private void SendStopLook()
+        {
+            var interacts = currentCollider.gameObject.GetComponents<IFPSLookAt>();
+            {
+                foreach (var interact in interacts)
+                {
+                    interact?.OnStopLook();
+                }
             }
         }
     }
+    
+    public interface IFPSInteract
+    {
+        void OnInteract();
+    }
+    
+    public interface IFPSLookAt
+    {
+        void OnLook();
+        void OnStopLook();
+    }
+
 }
 
-public interface IFPSInteract
-{
-    void OnInteract();
-}
-
-public interface IFPSLookAt
-{
-    void OnLook();
-    void OnStopLook();
-}
--- Unity/Level Design/Assets/Scripts/OutlineOnLook.cs
+++ Unity/Level Design/Assets/Scripts/OutlineOnLook.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using genaralskar.FPSInteract;
+
 [RequireComponent(typeof(Outline))]
 public class OutlineOnLook : MonoBehaviour, IFPSLookAt
 {
