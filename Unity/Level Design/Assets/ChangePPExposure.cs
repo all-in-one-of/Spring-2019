ebc35d563517f503e73345be6c5d02c8e80eecbd
Collin <genaralskar1@gmail.com>
2019-02-22 17:13:39 -0700

    update unity version, bake lights, set dress

---
+++ Unity/Level Design/Assets/ChangePPExposure.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class ChangePPExposure : MonoBehaviour
+{
+    public PostProcessVolume volume;
+
+    private ColorGrading colorGradingLayer;
+
+    private void Start()
+    {
+        volume.profile.TryGetSettings(out colorGradingLayer);
+    }
+
+    public void ChangeExposure(float newExposure)
+    {
+        colorGradingLayer.postExposure.value = newExposure;
+    }
+}
